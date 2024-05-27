c12a35edd96a151447ecb20a7ae3e6ef4f4f77b8
zofia janas <zoska91@wp.pl>
2024-05-27 16:37:59 +0200

    Add scubscription

---
+++ models/subscription.ts
@@ -0,0 +1,27 @@
+import mongoose, { Types } from 'mongoose';
+
+export interface ISubscription extends Document {
+  userId: Types.ObjectId;
+  endpoint: string;
+  keys: {
+    p256dh: string;
+    auth: string;
+  };
+}
+
+const SubscriptionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  endpoint: String,
+  keys: {
+    p256dh: String,
+    auth: String,
+  },
+});
+
+export const SubscriptionModel = mongoose.model<ISubscription>(
+  'Subscription',
+  SubscriptionSchema
+);

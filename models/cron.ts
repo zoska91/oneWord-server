6c231996005150007d5b67f3c41f084691103ab9
zofia janas <zoska91@wp.pl>
2024-05-30 11:13:35 +0200

    fix cron

--- models/cron.ts
+++ models/cron.ts
@@ -4,12 +4,14 @@ export interface ICron extends Document {
   userId: string;
   cronId: string;
   createdDate: string;
+  time: string;
 }
 
 const cronSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true },
   createdDate: { type: Date, default: new Date() },
   cronId: { type: String, requires: true },
+  time: { type: String, requires: true },
 });
 
 export const CronModel = mongoose.model<ICron>('Cron', cronSchema);

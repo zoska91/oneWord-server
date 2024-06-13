78dee80ad8747def8158971ac1feb84ab853756d
zofia janas <zoska91@wp.pl>
2024-06-13 14:43:40 +0200

    add limit notifications in settings

--- validation/settings.ts
+++ validation/settings.ts
@@ -6,12 +6,15 @@ export const userSettingsSchema = yup.object({
     isBreak: yup.bool(),
     isSummary: yup.bool(),
     summaryDay: yup.number(),
-    notifications: yup.array().of(
-      yup.object({
-        time: yup.string(),
-        type: yup.string(),
-      })
-    ),
+    notifications: yup
+      .array()
+      .of(
+        yup.object({
+          time: yup.string(),
+          type: yup.string(),
+        })
+      )
+      .max(5),
     languageToLearn: yup.number(),
     baseLanguage: yup.number(),
   }),
